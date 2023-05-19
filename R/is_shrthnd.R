55d43793dc2b6b3fceae8a63f11798930d7ee287
Matt Kerlogue <matt.kerlogue@me.com>
2023-05-19 01:55:18 +0100

    Add tbl predicate

--- R/is_shrthnd.R
+++ R/is_shrthnd.R
@@ -1,9 +1,10 @@
-#' shrthnd predicates
+#' Test if an object is using shrthnd
 #'
 #' The `is_shrthnd_*` family of functions test whether a vector is either a
 #' `shrthnd_num()`, or a `shrthnd_list()`. `is_shrthnd_integer()` and
 #' `is_shrthnd_double()` test whether an object is a `shrthnd_num()` vector
-#' and whether the underlying data type is an `integer()` or a `double()`
+#' and whether the underlying data type is an `integer()` or a `double()`.
+#' `is_shrthnd_tbl()` test whether an object is a `shrthnd_tbl()` tibble.
 #'
 #' @param x An object to be tested
 #'
@@ -25,6 +26,10 @@
 #'
 #' sh_l <- shrthnd_list(sh_x)
 #' is_shrthnd_list(sh_l)
+#'
+#' tbl <- tibble::tibble(x = x, sh_x = sh_x)
+#' sh_tbl <- shrthnd_tbl(tbl, title = "Example table")
+#' is_shrthnd_tbl(sh_tbl)
 is_shrthnd_num <- function(x) {
   inherits(x, "shrthnd_num")
 }
@@ -55,4 +60,30 @@ is_shrthnd_double <- function(x) {
 #' @rdname is_shrthnd
 is_shrthnd_list <- function(x) {
   inherits(x, "shrthnd_list")
+}
+
+#' @export
+#' @rdname is_shrthnd
+is_shrthnd_tbl <- function(x) {
+  inherits(x, "shrthnd_tbl")
+}
+
+# internal check functions ------------------------------------------------
+
+chk_shrthnd_num <- function(x) {
+  if (!is_shrthnd_num(x)) {
+    cli::cli_abort("{.arg x} must be a {.fun shrthnd::shrthnd_num} object")
+  }
+}
+
+chk_shrthnd_list <- function(x) {
+  if (!is_shrthnd_list(x)) {
+    cli::cli_abort("{.arg x} must be a {.fun shrthnd::shrthnd_list} object")
+  }
+}
+
+chk_shrthnd_tbl <- function(x) {
+  if (!is_shrthnd_tbl(x)) {
+    cli::cli_abort("{.arg x} must be a {.fun shrthnd::shrthnd_tbl} object")
+  }
 }
