d9bb4563c91596024e6f67c958b5991214296d50
Matt Kerlogue <matt.kerlogue@me.com>
2023-05-20 13:11:45 +0100

    Split out coercion tests

---
+++ tests/testthat/test-coercion.R
@@ -0,0 +1,23 @@
+x <- c("12", "34.567", "[c]", "NA", "56.78[e]", "78.9", "90.123[e]")
+y <- c("12", "34", "[c]", "NA", "56[e]", "78", "90[e]")s
+sh_x <- shrthnd_num(x)
+sh_y <- shrthnd_num(y)
+
+test_that("sh_num to numeric", {
+  expect_type(as.numeric(sh_x), "double")
+  expect_length(as.numeric(sh_x), 7)
+  expect_equal(as.numeric(sh_x), c(12, 34.567, NA, NA, 56.78, 78.9, 90.123))
+  expect_type(as.numeric(sh_y), "double")
+  expect_length(as.numeric(sh_y), 7)
+  expect_equal(as.numeric(sh_y), c(12, 34, NA, NA, 56, 78, 90))
+})
+
+test_that("sh_num to character", {
+  expect_type(as.character(sh_x), "character")
+  expect_length(as.character(sh_x), 7)
+  expect_equal(as.character(sh_x),
+               c("12", "34.567", NA, NA, "56.78", "78.9", "90.123"))
+  expect_type(as.character(sh_y), "character")
+  expect_length(as.character(sh_y), 7)
+  expect_equal(as.character(sh_y), c("12", "34", NA, NA, "56", "78", "90"))
+})
--- tests/testthat/test-num.R
+++ tests/testthat/test-num.R
@@ -23,25 +23,6 @@ test_that("sh_int generation", {
   expect_snapshot(tibble::tibble(y = y, sh_y = sh_y))
 })
 
-test_that("sh_num to numeric", {
-  expect_type(as.numeric(sh_x), "double")
-  expect_length(as.numeric(sh_x), 7)
-  expect_equal(as.numeric(sh_x), c(12, 34.567, NA, NA, 56.78, 78.9, 90.123))
-  expect_type(as.numeric(sh_y), "double")
-  expect_length(as.numeric(sh_y), 7)
-  expect_equal(as.numeric(sh_y), c(12, 34, NA, NA, 56, 78, 90))
-})
-
-test_that("sh_num to character", {
-  expect_type(as.character(sh_x), "character")
-  expect_length(as.character(sh_x), 7)
-  expect_equal(as.character(sh_x),
-               c("12", "34.567", NA, NA, "56.78", "78.9", "90.123"))
-  expect_type(as.character(sh_y), "character")
-  expect_length(as.character(sh_y), 7)
-  expect_equal(as.character(sh_y), c("12", "34", NA, NA, "56", "78", "90"))
-})
-
 test_that("shrthnd_num errors", {
   expect_error(shrthnd_num(1), regexp = "`x` must be a character vector")
   expect_error(shrthnd_num(list()), regexp = "`x` must be a character vector")
