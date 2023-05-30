b3d6beb08945abc8c5172097354b3d832380f118
Matt Kerlogue <matt.kerlogue@me.com>
2023-05-31 00:47:27 +0100

    handle rlang/tidyr weirdness

---
+++ R/globals.R
@@ -0,0 +1,6 @@
+
+# {rlang} dot-data pronoun needs declaring
+utils::globalVariables(".data")
+
+# `value` from find_notes and `guess_notes` needs declaring for some reason
+utils::globalVariables("value")
--- R/imports.R
+++ R/imports.R
@@ -1,5 +1,7 @@
 
 #' @import pillar
 #' @import vctrs
+#' @importFrom rlang `!!`
 #' @importFrom rlang `!!!`
+#' @importFrom rlang `:=`
 NULL
